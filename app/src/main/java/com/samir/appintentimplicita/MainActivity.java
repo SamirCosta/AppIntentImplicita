74c2e5b09b09970632a6a4df529adfdc0885dcce
“SamirCosta git config --global user.email “samircfaduli@gmail.com <65309047+SamirCosta@users.noreply.github.com>
2020-06-04 23:04:17 -0300

    cam

--- app/src/main/java/com/samir/appintentimplicita/MainActivity.java
+++ app/src/main/java/com/samir/appintentimplicita/MainActivity.java
@@ -29,4 +29,9 @@ public class MainActivity extends AppCompatActivity {
         startActivity(intent);
     }
 
+    public void abrirCam(View view){
+        Intent intent = new Intent(MainActivity.this, CamActivity.class);
+        startActivity(intent);
+    }
+
 }
