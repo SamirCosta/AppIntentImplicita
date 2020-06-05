fede1b8011f46e2f5b7d415a565d72dff405a157
“SamirCosta git config --global user.email “samircfaduli@gmail.com <65309047+SamirCosta@users.noreply.github.com>
2020-06-05 14:44:16 -0300

    cam ft

--- app/src/main/java/com/samir/appintentimplicita/CamActivity.java
+++ app/src/main/java/com/samir/appintentimplicita/CamActivity.java
@@ -2,13 +2,57 @@ package com.samir.appintentimplicita;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
 public class CamActivity extends AppCompatActivity {
+    private static final int CAPTURAR_IMAGEM = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cam);
     }
+
+    public void capturarImagem(View v) {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(intent, CAPTURAR_IMAGEM);
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAPTURAR_IMAGEM) {
+            if (resultCode == RESULT_OK) {
+                Bundle extras = data.getExtras();
+                Bitmap imageBitmap = (Bitmap) extras.get("data");
+                ImageView imageView = new ImageView(this);
+                imageView.setImageBitmap(imageBitmap);
+                LinearLayout ln = (LinearLayout)findViewById(R.id.layoutLinear);
+                ln.addView(imageView);
+                mostrarMensagem("Imagem capturada!");
+            } else {
+                mostrarMensagem("Imagem não capturada!");
+            }
+        }
+
+    }
+
+    private void mostrarMensagem(String msg){
+        Toast.makeText(this, msg,
+                Toast.LENGTH_LONG)
+                .show();
+    }
+
 }
